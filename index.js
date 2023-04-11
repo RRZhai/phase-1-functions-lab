15b8677023ff506a4c1999b0286f5bec454b9011
Shiyao Zhai <zsshiyaozhai@gmail.com>
2023-04-10 22:56:29 -0500

    passed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+let streetNum;
+function distanceFromHqInBlocks(streetNum){
+    return Math.abs(streetNum - 42);
+}
+
+function distanceFromHqInFeet(streetNum){
+    return distanceFromHqInBlocks(streetNum) * 264;
+}
+
+let start;
+let end;
+function distanceTravelledInFeet(start, end){
+    return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice(start, end){
+    if (distanceTravelledInFeet(start, end) <= 400){
+        return 0;
+    } else if (distanceTravelledInFeet(start, end) <= 2000){
+        return (distanceTravelledInFeet(start, end) - 400) *0.02;
+    } else if (distanceTravelledInFeet(start, end) <= 2500){
+        return 25;
+    } else if (distanceTravelledInFeet(start, end) > 2500){
+        return 'cannot travel that far';
+    }
+}
